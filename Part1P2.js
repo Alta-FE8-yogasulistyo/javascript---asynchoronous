d7a16f6ecd2bdb3ae7ed82f5c3bba8a041cfe5ca
Yoga Sulistyo <kgsyoga1997@gmail.com>
2022-08-27 23:53:20 +0700

    Part2

---
+++ Part1P2.js
@@ -0,0 +1,25 @@
+const clothes = {
+  item: "clothes",
+  price: 15000,
+  time: 3000,
+};
+
+const pants = {
+  item: "pants",
+  price: 25000,
+  time: 7000,
+};
+
+const hat = {
+  item: "hat",
+  price: 22000,
+  time: 2000,
+};
+
+const shoes = {
+  item: "shoes",
+  price: 46000,
+  time: 10000,
+};
+
+function buyApparel(money, objitem, callback) {}
